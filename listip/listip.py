5aa5ca7e88260a7a720c7cc804a51e0426881bb8
p7e4 <44750087+p7e4@users.noreply.github.com>
2020-11-06 15:14:43 +0800

    Create listip.py

---
+++ listip/listip.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+from ipaddress import ip_network, ip_address
+
+def listip(raw):
+    ips = []
+    for r in raw.replace(" ", "").split(","):
+        if "/" in r:
+            for i in ip_network(r, strict=False).hosts():
+                ips.append(str(i))
+        elif "-" in r:
+            start, end = [ip_address(i) for i in r.split("-")]
+            if start >= end:
+                start, end = end, start
+            while start <= end:
+                if start.version == 6 or not int(start) % 256 in [0, 255]:
+                    ips.append(str(start))
+                start += 1
+        else:
+            ips.append(str(ip_address(r)))
+
+    return ips
+
+if __name__ == '__main__':
+    for i in listip("192.168.1.1, 192.168.0.1/30, 192.168.2.1-192.168.2.10"):
+        print(i)
+
+
+
+
