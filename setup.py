3e6c37670585a8186428d3ff89338892e7b8ba02
p7e4 <44750087+p7e4@users.noreply.github.com>
2020-11-15 22:04:10 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="listip",
-    version="0.0.1",
+    version="0.0.2",
     author="p7e4",
     author_email="p7e4@qq.com",
     description="Expand ip segment/cidr, for scanners",
