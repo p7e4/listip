69d78f67d1514323fde7e6ac511c30591c23de64
p7e4 <44750087+p7e4@users.noreply.github.com>
2020-11-15 22:02:46 +0800

    Update listip.py

--- listip/listip.py
+++ listip/listip.py
@@ -2,7 +2,7 @@
 
 from ipaddress import ip_network, ip_address
 
-def listip(raw):
+def expand(raw):
     ips = []
     for r in raw.replace(" ", "").split(","):
         if "/" in r:
@@ -18,11 +18,19 @@ def listip(raw):
                 start += 1
         else:
             ips.append(str(ip_address(r)))
-
     return ips
 
+def listip(raw, exclude=""):
+    ips = expand(raw)
+    if exclude:
+        ips = list(set(ips) - set(expand(exclude)))
+
+    ips.sort()
+    return ips
+
+
 if __name__ == '__main__':
-    for i in listip("192.168.1.1, 192.168.0.1/30, 192.168.2.1-192.168.2.10"):
+    for i in listip("192.168.1.1, 192.168.0.1/30, 192.168.2.1-192.168.2.10", exclude="192.168.1.1, 192.168.2.1/24"):
         print(i)
 
 
